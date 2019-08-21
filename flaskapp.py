0c578f84078832bab5d30bfc54c44e5bec5b3775
upworka0 <upworka0@gmail.com>
2019-08-21 20:31:20 +0200

    update 1

--- flaskapp.py
+++ flaskapp.py
@@ -1,30 +1,34 @@
 from flask import Flask, request, make_response, jsonify
 import os
+import requests
+
 app = Flask(__name__)
+
+count = 0
 
 @app.route('/')
 def index():
     return 'Hello World!'
 
-
 # function for responses
 def results():
+    global count
     # build a request object
     req = request.get_json(force=True)
-
     # fetch action from json
     action = req.get('queryResult').get('action')
-    print("Action is %s " % action)
+    print(req)
     # return a fulfillment response
-    return {'fulfillmentText': 'This is a response from webhook.'}
+    count = count + 1
+    return {'fulfillmentText': 'This is a Question %s' % count}
 
 
 # create a route for webhook
-@app.route('/webhook')
+@app.route('/webhook', methods=['POST'])
 def webhook():
     return make_response(jsonify(results()))
 
 
 # run the app
 if __name__ == '__main__':
-   app.run()
+    app.run()
