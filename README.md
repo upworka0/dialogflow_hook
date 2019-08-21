8ef8c601c99fc543704bf6dfc66674cc46aea510
upworka0 <upworka0@gmail.com>
2019-08-21 18:20:02 +0200

    make it better

---
+++ README.md
@@ -0,0 +1,32 @@
+Dialogflow FAQ bot
+==================
+
+Dialogflow FAQ ChatBot interacting with database
+
+
+Requirements
+------------
+
+- Flask (`pip install flask`).
+
+Installation
+------------
+
+You can create a virtual environment and install the required packages with the following commands:
+
+    $ virtualenv venv
+    $ . venv/bin/activate
+    (venv) $ pip install -r requirements.txt
+
+
+Running the Flask app
+--------------------
+
+With the virtual environment activated you can `cd` into any of the examples and run the main script.
+
+Running :
+
+    (venv) $ python flaskapp.py
+
+
+
---
+++ flaskapp.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, make_response, jsonify
+import os
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+
+# function for responses
+def results():
+    # build a request object
+    req = request.get_json(force=True)
+
+    # fetch action from json
+    action = req.get('queryResult').get('action')
+
+    # return a fulfillment response
+    return {'fulfillmentText': 'This is a response from webhook.'}
+
+
+# create a route for webhook
+@app.route('/webhook')
+def webhook():
+    return make_response(jsonify(results()))
+
+
+# run the app
+if __name__ == '__main__':
+   app.run()
