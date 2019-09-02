7fbf6edacc71e1e39f38ad4b52f326f270609de7
upworka0 <upworka0@gmail.com>
2019-09-02 19:53:47 +0200

    change db server

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # DB_URL= 'sqlite:///database.sqlite'
 # DB_URL='mysql://root:password@localhost/udemy?charset=utf8mb4'
-DB_URL='mysql://root:password@167.71.242.245/udemy?charset=utf8mb4'
+DB_URL='mysql://root:@,jYd*%x?622kMn6MvW78zdTy92cmx28@159.65.190.50/udemy?charset=utf8mb4'
 ACCESS_TOKEN = ''
