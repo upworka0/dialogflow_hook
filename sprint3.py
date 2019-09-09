36dd20302439e73abd7bd974685246e444c755b2
upworka0 <upworka0@gmail.com>
2019-09-09 06:20:05 +0200

    added test funtion

--- sprint3.py
+++ sprint3.py
@@ -3,6 +3,9 @@ from sqlalchemy.orm import sessionmaker
 from models import Course, Base, Question, Answer
 from urllib.parse import urlencode
 import json
+import requests
+from pprint import pprint
+
 from config import DB_URL, ACCESS_TOKEN
 import logging
 logging.basicConfig(filename='log.log', level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
@@ -17,9 +20,56 @@ session = DBSession()
 
 class UdemyAnswer:
     DB_CONFIG = 'mysql://root:password@localhost/udemy?charset=utf8mb4'
-    BASE_URL = "https://www.udemy.com/instructor-api/v1/courses/{course_id}/questions/{question_id}/replies/"
+    """
+        https://www.udemy.com/developers/instructor/methods/post-api-course-question-replies-list/
+        POST /instructor-api/v1/courses/{course_id}/questions/{question_id}/replies/
+    """
+
+    BASE_URL = "https://www.udemy.com/instructor-api/v1/courses/{0}/questions/{1}/replies/"
     access_token = ""
 
     def __init__(self, access_token):
         self.access_token = access_token
 
+    def get_headers(self):
+        return {
+            "Authorization": "bearer %s" % self.access_token,
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+
+    def _answer(self, course_id, question_id, user_id, answer_text):
+        payload = {
+            "course_id": course_id,
+            "question_id": question_id,
+            "body": answer_text
+        }
+
+        url = self.BASE_URL.format(course_id, question_id)
+        logging.info("REQUEST to %s " % url)
+        logging.info("REQUEST payload : %s" % json.dumps(payload))
+        print(url)
+
+        res = requests.post(url, data=payload, headers=self.get_headers())
+        response = res.json()
+        if res.status_code > 200:
+            logging.error("Error: %s " % response['detail'])
+        else:
+            logging.info("Response : %s" % json.dumps(response))
+        pprint(response)
+
+
+if __name__ == '__main__':
+    udemy = UdemyAnswer(access_token='test')
+    udemy._answer(1,1,1,"test")
+
+
+    """
+        Testing:
+            >>>            
+            course = "x01JUY15zH9s9_lzjMlyuxG-g=="
+            question_id = "x01Yx324gCaA2VmKGJZJPMqRQ=="
+            answer = "Attribute Error is no problem"
+            user_id = None
+            udemy._answer(course, question_id, user_id, answer)
+    """
