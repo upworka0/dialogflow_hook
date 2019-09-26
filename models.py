7938ea92ce48bfbccabf01600ce45e9dee253592
upworka0 <upworka0@gmail.com>
2019-09-26 13:25:47 +0200

    change model

--- models.py
+++ models.py
@@ -1,9 +1,10 @@
 from config import DB_URL
-from sqlalchemy import Column, ForeignKey, Integer, String, Text, DateTime, Boolean
+from sqlalchemy import Column, ForeignKey, Integer, String, Text, DateTime, Boolean, TIMESTAMP, text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 import datetime
+from sqlalchemy import func
 
 Base = declarative_base()
 
@@ -13,9 +14,9 @@ class Course(Base):
     # Here we define columns for the table courses
     # Notice that each column is also a normal Python instance attribute.
     id = Column(String(250), primary_key=True)
-    _class = Column(String(50), nullable=False)
-    title = Column(String(250), nullable=False)
-    url = Column(String(250), nullable=False)
+    _class = Column(String(50), nullable=False)     # class id
+    title = Column(String(250), nullable=False)     # title of course
+    url = Column(String(250), nullable=False)       # url of course
 
     def __str__(self):
         return self.id
@@ -26,17 +27,17 @@ class Question(Base):
     # Here we define columns for the table questions.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True, autoincrement=True)
-    question_id = Column(String(250), unique=True)
-    title = Column(String(250), nullable=False)
-    body = Column(Text())
-    num_replies = Column(Integer, nullable=True)
-    num_follows = Column(Integer, nullable=True)
-    num_reply_upvotes = Column(Integer, nullable=True)
-    created = Column(String(20))
-    course_id = Column(String(250), ForeignKey('courses.id'))
-    course = relationship(Course)
-    course_num = Column(Integer, nullable=False)
-    replied = Column(Boolean, default=False)
+    question_id = Column(String(250), unique=True)              # id of question
+    title = Column(String(250), nullable=False)                 # title of question
+    body = Column(Text())                                       # body of question
+    num_replies = Column(Integer, nullable=True)                # count of replies
+    num_follows = Column(Integer, nullable=True)                # count of follows
+    num_reply_upvotes = Column(Integer, nullable=True)          # count of upvotes replied
+    created = Column(String(20))                                # created date
+    course_id = Column(String(250), ForeignKey('courses.id'))   # id of course table
+    course = relationship(Course)                               # foreignkey of course
+    course_num = Column(Integer, nullable=False)                # int id of course
+    replied = Column(Boolean, default=False)                    # flag for reply
 
     def __str__(self):
         return self.title
@@ -46,13 +47,29 @@ class Answer(Base):
     __tablename__ = "answers"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    response = Column(Text())
-    question_id = Column(Integer, ForeignKey('questions.id'))
-    question = relationship(Question)
-    created = Column(DateTime, onupdate=datetime.datetime.now)
+    response = Column(Text())                                   # Answer text
+    question_id = Column(Integer, ForeignKey('questions.id'))   # id of qeustion
+    question = relationship(Question)                           # foreignkey of question
+    created = Column(DateTime(), nullable=False,
+                        server_default=func.current_timestamp(),
+                        server_onupdate=func.current_timestamp())  # created date
 
     def __str__(self):
         return self.response
+
+class Matrix(Base):
+    __tablename__ = 'matrix'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    course_id = Column(String(250), ForeignKey('courses.id'))   # id of course table
+    course = relationship(Course)                               # foreignkey of course
+    course_num = Column(Integer, nullable=False)                # int id of course
+    num_replied = Column(Integer, nullable=True)                # count of answered questions
+    num_total = Column(Integer, nullable=True)                  # count of total obtained questions
+    created = Column(DateTime(), nullable=False,
+                     server_default=func.current_timestamp(),
+                     server_onupdate=func.current_timestamp())  # created date
+
 
 # Create an engine that stores data in the local directory's
 # sqlalchemy_example.db file. 'sqlite:///sqlalchemy_example.db'
